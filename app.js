4f9d774c3e4f5114c1c4de0d27186c72445ab361
Arshman Afzal <arshman3@gmail.com>
2022-08-28 01:14:00 +0500

    updates

--- app.js
+++ app.js
@@ -1,72 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const fs = require("fs");
-let Client = require('ssh2-sftp-client');
 const app = express();
-const multer = require("multer");
-const { json } = require('body-parser');
-const storage = multer.diskStorage({
-    destination: function( req, file, cb) {
-        cb(null, './ovpns')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname)
-    }
-});
-let sftp = new Client();
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const serverRoutes = require('./routes/serverRoutes');
 
-const upload = multer({
-    storage: storage
-});
+app.use(express.json());
 
-app.get("/", upload.array("ovpns", 2), (req, res) => {
-    console.log(req.files);
-    res.send("The files have been uploaded");
-});
-
-// app.get("/", (req, res) => {
-//     res.send("Hello this is working");
-// })
-
-app.get("/getOVPN/:ovpnname", (req, res) => {
-    const filePath  = "./ovpns/" + req.params.ovpnname + ".ovpn";
-    fs.readFile(filePath, (error, data) => {
-        if(error)
-        {
-            console.log(error);
-            throw error;
-        }
-        res.send(data.toString());
-    })
-    // let rawdata = fs.readFileSync(filePath);
-    // console.log(rawdata);
-    // let data = JSON.parse(rawdata);
-    // res.send(rawdata);
-    // const file = './ovpns/ovpnname.ovpn';
-    // res.download(filePath);
-});
-
-app.get("/createNewServer", (req, res) => {
-    try{
-        sftp.connect({
-            host: "carflexrenalandleasing.com",
-            port: '22',
-            username: 'root',
-            password: 'Arain@123'
-        }).then(() => {
-            res.send(sftp.list('/pathname'));
-        }).then(data => {
-            console.log(data, 'the data info');
-        }).catch(err => {
-            console.log(err, "this is error message");
-        });
-    }
-   catch{
-    console.log("Error generated");
-   }
+app.use(cors(), (req, res, next) => {
+    next()
 })
 
+mongoose.connect('mongodb://localhost/local', () => {
+    console.log("Connected to database");
+});
+
+app.use(express.json({ limit: '1000mb' }));
+
+app.use('/auth', authRoutes);
+app.use('/server', serverRoutes);
 
 app.listen(3000, () => {
-    console.log("The app is running on 3000");
-})
+    console.log("Server running at 3000");
+});
